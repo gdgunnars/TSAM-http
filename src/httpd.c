b40dad85deaaa842d52d042f918b1001383c0478
Hlynur Stefánsson <hlynurs15@ru.is>
2017-10-09 04:32:31 +0000

    make handin

--- src/httpd.c
+++ src/httpd.c
@@ -1,34 +1,25 @@
-/* A TCP echo server.
+/* $Id: httpd.c,v 1.0 2017/09/10 04:30:00 hpa Exp $ */
+/* ----------------------------------------------------------------------- *
+ *   
+ *   Copyright 2017 
+ *		Guðjón Steinar Sverrisson
+ *		Gunnar Davíð Gunnarsson
+ *		Hlynur Stefánsson
+  * - All Rights Reserved
  *
- * Receive a message on port 32000, turn it into upper case and return
- * it to the sender.
+ *   This program is free software available under the same license
+ *   as the "OpenBSD" operating system, distributed at
+ *   http://www.openbsd.org/.
  *
- * Copyright (c) 2016, Marcel Kyas
- * All rights reserved.
+ * ----------------------------------------------------------------------- */
+
+/*
+ * httpd.c
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of Reykjavik University nor the
- *       names of its contributors may be used to endorse or promote products
- *       derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL MARCEL
- * KYAS NOR REYKJAVIK UNIVERSITY BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE.
+ * A Simple HTTP Server that accepts GET, POST and HEAD and returns an in memory
+ * generated HTML5 page that contains the requested URL, client ip and port and
+ * the body of the request (if the request was POST).
+ *                                                               d(-_-)b
  */
 
 #include <sys/socket.h>
